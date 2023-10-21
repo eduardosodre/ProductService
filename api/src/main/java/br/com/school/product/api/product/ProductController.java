2c89fdc55252c165f65c1d5415d5e544495762e0
Eduardo Nicolini Sodre da Silva <eduardo_nsodre@hotmail.com>
2023-10-21 13:00:15 -0300

    Video 9

--- api/src/main/java/br/com/school/product/api/product/ProductController.java
+++ api/src/main/java/br/com/school/product/api/product/ProductController.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,30 @@ public class ProductController {
         final var savedProduct = service.createProduct(productEntity);
         final var productResponse = ProductResponse.fromEntity(savedProduct);
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getById(@PathVariable String id) {
+        final var product = service.getProductById(id);
+        final var response = ProductResponse.fromEntity(product);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+        @PathVariable String id,
+        @RequestBody ProductRequest request) {
+        final var productFromBd = service.getProductById(id);
+        final var productToUpdate = request.toEntity(id);
+        final var savedProduct = service.updateProduct(productFromBd, productToUpdate);
+        final var productResponse = ProductResponse.fromEntity(savedProduct);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductResponse> deleteById(@PathVariable String id) {
+        service.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
