df896d567ef6e3d4990eae160f6316c0a5f6df04
Eduardo Nicolini Sodre da Silva <eduardo_nsodre@hotmail.com>
2023-10-23 13:52:30 -0300

    Video 13

--- api/src/main/java/br/com/school/product/api/product/ProductController.java
+++ api/src/main/java/br/com/school/product/api/product/ProductController.java
@@ -1,6 +1,10 @@
 package br.com.school.product.api.product;
 
 import br.com.school.product.domain.product.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +26,8 @@ public class ProductController {
 
     private final ProductService service;
 
+    @Operation(summary = "create a new product")
+    @ApiResponse(responseCode = "201", description = "product created successfully")
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(
         @RequestBody ProductRequest request) {
@@ -31,6 +37,9 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
 
+    @Operation(summary = "Get a product by ID")
+    @ApiResponse(responseCode = "200", description = "Product found")
+    @ApiResponse(responseCode = "404", description = "Product not found")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getById(@PathVariable String id) {
         final var product = service.getProductById(id);
@@ -38,6 +47,9 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Update an existing product")
+    @ApiResponse(responseCode = "200", description = "Product updated successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Product not found")
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> updateProduct(
         @PathVariable String id,
@@ -49,12 +61,17 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productResponse);
     }
 
+    @Operation(summary = "Delete a product by ID")
+    @ApiResponse(responseCode = "204", description = "Product deleted successfully")
+    @ApiResponse(responseCode = "404", description = "Product not found")
     @DeleteMapping("/{id}")
     public ResponseEntity<ProductResponse> deleteById(@PathVariable String id) {
         service.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get all products")
+    @ApiResponse(responseCode = "200", description = "List of products")
     @GetMapping
     public ResponseEntity<Page<ProductResponse>> getAllProducts(Pageable pageable) {
         final var products = service.findAllProducts(pageable);
